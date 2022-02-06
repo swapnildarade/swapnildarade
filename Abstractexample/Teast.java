8f5703518934377de3b76f26b0f82ae6d7d1021c
swapnildarade <98216429+swapnildarade@users.noreply.github.com>
2022-02-06 17:01:01 +0530

    Update Teast.java

--- Abstractexample/Teast.java
+++ Abstractexample/Teast.java
@@ -4,7 +4,15 @@ public class Teast {
 
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+        // Rectangle test
+        double width = 5, length = 7;
+        Shape Rectangle = new Rectangle(width, length);
+        System.out.println("Rectangle width: " + width + " and length: " + length+ "\nResulting area: " + Rectangle.area());
+
+        // Circle test
+        double r = 5;
+        Shape Circle = new Circle(r);
+        System.out.println("Circle radius: " + r+ "\nResulting Area: " + Circle.area());
 	}
 }
