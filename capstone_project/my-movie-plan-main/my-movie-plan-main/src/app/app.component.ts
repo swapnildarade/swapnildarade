7634d945f79bb8bdc4ebecdf08142c53cb53ea9b
swapnildarade <swapnildarade0123@gmail.com>
2022-04-04 12:20:02 +0530

    capstone

---
+++ capstone_project/my-movie-plan-main/my-movie-plan-main/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { GlobalConstants } from './commons/global-constants';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title: string = GlobalConstants.APP_NAME;
+  url="./assets/image/movie1.jpg";
+}
