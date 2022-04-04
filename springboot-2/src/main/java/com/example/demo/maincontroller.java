6ccb7a3c4296b3f664c05fd591c24364dbf3af63
swapnildarade <swapnildarade0123@gmail.com>
2022-04-04 10:59:04 +0530

    phase 5 main

---
+++ springboot-2/src/main/java/com/example/demo/maincontroller.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class maincontroller {
+
+  @RequestMapping("/")
+  public String index() {
+    return "index.html";
+  }
+}
+
+
